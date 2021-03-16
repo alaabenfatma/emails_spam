10bfea380792c3255f54a952e716aef8255b502a
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-03-16 01:11:06 +0100

    Load data Clean the data

---
+++ spam_classifier.R
@@ -0,0 +1,33 @@
+## Read the emails dataset
+emails = read.csv("emails.csv")
+
+"
+  This dataset contains a lot of useless columns. As a matter of fact,
+  every columns that goes past the second one has to be removed
+"
+clean_emails <- emails[, -c(3:ncol(emails))] # delete columns 5 through 7
+
+"
+  Now, even though we have gotten rid of the non-used columns, we can still
+  see some noise in the labels.
+  Labels have to be either 1 (spam) or 0(non spam); however, we can see some
+  empty or unreadable text in some of the label column.
+"
+
+# Remove rows with empty emails or empty label
+# Empty email
+cat("Dataset size:", dim(clean_emails))
+
+clean_emails <- clean_emails[!(clean_emails$text == ""), ]
+cat("Dataset size after removing empty emails:", dim(clean_emails))
+
+# Empty label
+clean_emails <- clean_emails[!(clean_emails$spam == ""), ]
+cat("Dataset size after removing empty labels:", dim(clean_emails))
+
+# Noise label
+clean_emails <- clean_emails[grepl("0|1", clean_emails$spam),]
+cat("Dataset size after removing bad labels:", dim(clean_emails))
+
+
+
